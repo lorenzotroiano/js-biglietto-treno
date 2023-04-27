5ab59854af34e2527aeb97ca47d8f0aff075b2a3
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-04-27 17:58:04 +0200

    finito

--- js/train.js
+++ js/train.js
@@ -20,16 +20,36 @@ console.log(userEta);
          //  sconto del 20% per i minorenni
          numSconto = 20;
          document.bgColor="yellow";
+
+
+         let img = document.createElement("img");
+        img.src = "https://www.fondazioneveronesi.it/uploads/2020/10/26/dieta-dentizione-svezzamento.jpg";
+        let src = document.getElementById("x");
+         src.appendChild(img);
     }
      else if (userEta >= 65) {
          //  sconto del 40% per over 65
         numSconto = 40;
         document.bgColor="red";
+
+
+        let img = document.createElement("img");
+        img.src = "https://st.depositphotos.com/2309453/3076/i/600/depositphotos_30761235-stock-photo-portrait-of-a-smiling-attractive.jpg";
+        let src = document.getElementById("x");
+         src.appendChild(img);
      }
      else {
         // prezzo normale
         numSconto = 0;
         document.bgColor="violet";
+
+
+
+        let img = document.createElement("img");
+        img.src = "https://images2.gazzettaobjects.it/methode_image/2018/12/10/Calcio/Foto%20Calcio%20-%20Trattate/31.0.1012834460-0050-U2021820495617iD-U3101123990664zRD-620x349@Gazzetta-Web_articolo.jpg?v=201812101702";
+        let src = document.getElementById("x");
+         src.appendChild(img);
+
      }
     
 
@@ -42,6 +62,18 @@ console.log(userEta);
      console.log(prezzoSconto);
      console.log(prezzoFinale);
     
+
+
+        // let img = document.createElement("img");
+        // img.src = "https://www.fondazioneveronesi.it/uploads/2020/10/26/dieta-dentizione-svezzamento.jpg";
+        // let src = document.getElementById("x");
+        //  src.appendChild(img);
+
+
+
+
+
+
     messaggio = "Il biglietto ti viene a costare $";
     
 
