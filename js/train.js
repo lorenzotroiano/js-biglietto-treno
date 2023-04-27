1e9f9c5167716c803ce1b48ef86d71999d2d6098
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-04-27 16:10:49 +0200

    inserito ndecimali

--- js/train.js
+++ js/train.js
@@ -1,5 +1,5 @@
 // generali
-let messaggio, numSconto1, numSconto2, prezzoSconto1, prezzoSconto2;
+let messaggio, numSconto1, numSconto2, prezzoSconto1, prezzoSconto2, nDecimali1, nDecimali2;
 
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
 let numKm = parseInt(prompt("Quanti km vuoi percorrere?"));
@@ -19,20 +19,32 @@ console.log(userEta);
     if (userEta <= 17) {
         numSconto1 = prezzoTicket * 20 / 100;
         prezzoSconto1 = prezzoTicket - numSconto1;
+        nDecimali1 = prezzoSconto1.toFixed(2);
     }
      else if (userEta >= 65) {
         numSconto2 = prezzoTicket * 40 / 100;
         prezzoSconto2 = prezzoTicket - numSconto2;
+        nDecimali2 = prezzoSconto2.toFixed(2);
      }
     //  sconto del 20% per i minorenni
     //  sconto del 40% per over 65
+
+
 
     console.log(numSconto1);
     console.log(numSconto2);
     console.log(prezzoSconto1);
     console.log(prezzoSconto2);
 
+    console.log(nDecimali1);
 
+    console.log(nDecimali2);
+     
+    //  nDecimali1 = prezzoSconto1.toFixed(2);
+    //  console.log(nDecimali1);
+
+    //  nDecimali2 = prezzoSconto2.toFixed(2);
+    //  console.log(nDecimali2);
 
     
     document.getElementById("train").innerHTML = "ciaoooo";
