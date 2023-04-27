4a44b66e2a0f3a258083231e1d97b4922094a01e
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-04-27 15:53:48 +0200

    completata parte di logica + calcolo

---
+++ js/train.js
@@ -0,0 +1,47 @@
+// generali
+let messaggio, numSconto1, numSconto2, prezzoSconto1, prezzoSconto2;
+
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
+let numKm = parseInt(prompt("Quanti km vuoi percorrere?"));
+console.log(numKm);
+
+
+// e l’età del passeggero.
+let userEta = parseInt(prompt("Quanti anni hai?"));
+console.log(userEta);
+
+// calcolare il prezzo totale del viaggio, secondo queste regole:
+    // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    let prezzoTicket = numKm * 0.21;
+    console.log(prezzoTicket);
+    
+    
+    if (userEta <= 17) {
+        numSconto1 = prezzoTicket * 20 / 100;
+        prezzoSconto1 = prezzoTicket - numSconto1;
+    }
+     else if (userEta >= 65) {
+        numSconto2 = prezzoTicket * 40 / 100;
+        prezzoSconto2 = prezzoTicket - numSconto2;
+     }
+    //  sconto del 20% per i minorenni
+    //  sconto del 40% per over 65
+
+    console.log(numSconto1);
+    console.log(numSconto2);
+    console.log(prezzoSconto1);
+    console.log(prezzoSconto2);
+
+
+
+    
+    document.getElementById("train").innerHTML = "ciaoooo";
+// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+
+
+
+
+
+
+
+
